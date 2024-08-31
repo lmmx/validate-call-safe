8143ac5bc0b1c4717dab0dc69ddbb4bffd9f3919
Louis Maddox <louismmx@gmail.com>
2024-08-31 11:17:56 +0100

    feat: add back the configurable model behaviour, add default ErrorModel

--- src/validate_call_safe/decorator.py
+++ src/validate_call_safe/decorator.py
@@ -1,35 +1,56 @@
+from __future__ import annotations
+
 from functools import wraps
-from typing import Type, TypeVar, Callable, Any, Union, overload
-from pydantic import BaseModel, ValidationError, validate_call
+from typing import TypeVar, Callable, Any, Union, overload
+from pydantic import BaseModel, ValidationError, validate_call, Json
 
 T = TypeVar("T", bound=BaseModel)
 R = TypeVar("R")
 
+class ErrorModel(BaseModel):
+    error_type: str
+    error_json: Json
+    error_str: str
+    error_repr: str
 
+
+# Decorator with brackets
 @overload
 def validate_call_safe(
-    error_model: Type[T],
+    error_model: type[T] = ErrorModel,
+    *,
+    config: ConfigDict | None = None,
+    validate_return: bool = False,
 ) -> Callable[[Callable[..., R]], Callable[..., Union[R, T]]]: ...
 
 
+# Decorator without brackets
 @overload
 def validate_call_safe(
-    error_model: Type[T], func: Callable[..., R]
+    func: Callable[..., R],
+    /,
 ) -> Callable[..., Union[R, T]]: ...
 
 
-def validate_call_safe(error_model: Type[T], func: Callable[..., R] = None):
+def validate_call_safe(
+    error_model_or_func: type[T] | Callable[..., R] = ErrorModel,
+    /,
+    *,
+    config: ConfigDict | None = None,
+    validate_return: bool = False,
+):
     """Decorator for validating function calls and handling errors safely.
 
     This decorator wraps the Pydantic validate_call decorator and captures any validation
     or other errors, passing them into a provided error model instead of allowing them to raise.
 
-    Usage may be either as a decorator with arguments `@validate_call_safe(ErrorModel)`
-    or as a plain decorator `@validate_call_safe(ErrorModel)(func)`.
+    Usage must be as a decorator with arguments `@validate_call_safe(ErrorModel, config=...)`.
 
     Args:
         error_model: A Pydantic model class to use for error reporting.
         func: The function to be decorated (optional, can be passed in decorator form).
+        config: Configuration for the Pydantic model (optional).
+        validate_return: Whether to validate the return value.
 
     Returns:
         The decorated function that returns either the original return type or the error model.
@@ -49,9 +70,22 @@ def validate_call_safe(error_model: Type[T], func: Callable[..., R] = None):
         result = int_func("not an int")  # Returns CustomErrorModel instance
         ```
     """
+    if error_model_or_func is ErrorModel:
+        # validate_call is used with empty brackets, and defaults to the ErrorModel
+        error_model = error_model_or_func
+        func = None
+    elif issubclass(error_model_or_func, BaseModel):
+        # validate_call is used with brackets, and a positional error_model was set
+        error_model = error_model_or_func
+        func = None
+        # TODO: of this logic is sound, this can be merged with previous condition
+    else:
+        # validate_call is used without brackets, and defaults to the ErrorModel
+        func = error_model_or_func
+        error_model = ErrorModel
 
     def validate(f: Callable[..., R]) -> Callable[..., Union[R, T]]:
-        validated_func = validate_call(f)
+        validated_func = validate_call(f, config=config)
 
         @wraps(f)
         def wrapper(*args: Any, **kwargs: Any) -> Union[R, T]:
