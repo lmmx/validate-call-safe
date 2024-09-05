b6dd9fb380f6ff0940dca1e378964ac8f7d19f60
Louis Maddox <louismmx@gmail.com>
2024-09-05 23:31:58 +0100

    feat: working Union of models error model validation

---
+++ examples/simple/default_error_model/union.py
@@ -0,0 +1,37 @@
+from typing import Literal
+from pydantic import BaseModel
+from validate_call_safe import validate_call_safe, ErrorModel
+from inline_snapshot import snapshot
+
+
+class A(BaseModel):
+    x: int
+    y: int
+
+class B(BaseModel):
+    x: str
+
+
+class C(BaseModel):
+    z: int
+
+
+class ValidnFail(BaseModel):
+    error_type: Literal["ValidationError"]
+
+class AttribFail(BaseModel):
+    error_type: Literal["AttributeError"]
+
+@validate_call_safe(ValidnFail | AttribFail, validate_body=True)
+def x_access(model: A | B | C) -> int:
+    """Input is assumed to have field `x: int`."""
+    return model.x
+
+
+A_xy_x = x_access(dict(x=1, y=2))  # Resolves to A(x: int, y: str)
+# -> ErrorModel(error_type='ValidationError', ...)
+
+B_fail = x_access(dict(x=1))  # No ! Resolves to B(x: str) not x: int
+# -> ErrorModel(error_type='ValidationError'
+
+C_fail = x_access(dict(z=3))  # No ! Resolves to C(z: int) not x: int
--- src/validate_call_safe/decorator.py
+++ src/validate_call_safe/decorator.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from functools import wraps
 from traceback import format_exc
-from typing import Annotated, Any, TypeVar, overload, get_origin, get_args
+import types
+from typing import Annotated, Any, TypeVar, overload, get_origin, get_args, Type, Union, _GenericAlias
 from collections.abc import Callable
 
 from pydantic import BaseModel, ConfigDict, TypeAdapter, ValidationError, validate_call
@@ -12,9 +13,16 @@ from .errors import ErrorModel
 T = TypeVar("T", bound=BaseModel)
 R = TypeVar("R")
 X = TypeVar("X", bound=BaseException)
+GenericType = Union[Type, _GenericAlias]
 
 __all__ = ("validate_call_safe",)
 
+def is_union(cls: GenericType) -> bool:
+    """Check if a class is a Union."""
+    # UnionType added in py3.10
+    if not hasattr(types, "UnionType"):
+        return get_origin(cls) is Union
+    return get_origin(cls) in [Union, types.UnionType]
 
 # Decorator with brackets
 @overload
@@ -92,8 +100,23 @@ def validate_call_safe(
             return isinstance(base_cls, type) and issubclass(base_cls, BaseModel)
         return False
 
+    def is_union_basemodel_subclasses(cls):
+        if is_union(cls):
+            all_models = all(
+                isinstance(base_cls, type) and issubclass(base_cls, BaseModel)
+                for base_cls in get_args(cls)
+            )
+            if all_models:
+                return True
+            else:
+                raise TypeError(f"Union argument to decorator must only contain models")
+        return False
+
     empty_brackets = error_model_or_func is ErrorModel
-    is_wrapped_model_cls = is_annotated_basemodel_subclass(error_model_or_func)
+    is_annotated_model_cls = is_annotated_basemodel_subclass(error_model_or_func)
+    is_model_cls_union = is_union_basemodel_subclasses(error_model_or_func)
+    # For Union, we'll use the default ErrorModel then dump into a TypeAdapter to decide
+    is_wrapped_model_cls = is_annotated_model_cls or is_model_cls_union
     pos_arg_is_cls = isinstance(error_model_or_func, type) or is_wrapped_model_cls
 
     # Annotated classes behave as the class (`__call__` method falls thru to model cls)
@@ -112,11 +135,15 @@ def validate_call_safe(
 
     # TODO: cache these TypeAdapters at module level to avoid recomputation latency
     # (would affect programs with multiple decorated classes with custom error models?)
-    if is_wrapped_model_cls:
+    if is_annotated_model_cls:
         # TypeAdapter triggers functional validators in Annotated metadata if present.
         # Pre-provision it here (upon # decorator creation, if I understand correctly?)
         # rather than delaying TypeAdapter creation until the wrapper function is run.
         error_model_validate = TypeAdapter(error_model).validate_python
+    elif is_model_cls_union:
+        # First parse into the default `ErrorModel`, then use a `TypeAdapter` on the
+        # dumped model output to re-parse as one of the `Union` model classes
+        error_model_validate = ErrorModel.model_validate
     else:
         # There is no `Annotated` metadata (so no potential functional validators),
         # so no need to use `TypeAdapter` just a regular `.model_validate()` method
@@ -154,6 +181,11 @@ def validate_call_safe(
                             error_tb=format_exc(),
                         ),
                     )
+                    # TODO: move this TypeAdapter construction up to a higher level:
+                    if is_model_cls_union:
+                        # We just validated into a temporary ErrorModel
+                        # Now validate using a TypeAdapter on the Union of models
+                        ret = TypeAdapter(error_model).validate_python(ret.model_dump())
                     if report and validate_return:
                         reporter(f"{func_name} -> {ret!r}")
             except extra_exceptions as e:
@@ -170,6 +202,11 @@ def validate_call_safe(
                             error_tb=format_exc(),
                         ),
                     )
+                    # TODO: move this TypeAdapter construction up to a higher level:
+                    if is_model_cls_union:
+                        # We just validated into a temporary ErrorModel
+                        # Now validate using a TypeAdapter on the Union of models
+                        ret = TypeAdapter(error_model).validate_python(ret.model_dump())
                     if report and validate_return:
                         reporter(f"{func_name} -> {ret!r}")
             else:
